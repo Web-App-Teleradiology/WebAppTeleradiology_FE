a0f402e5eb554ce7ae9c5541048af51548cf1204
kareraolivier <kareraolivier@gmail.com>
2023-11-01 12:53:00 +0200

    login

---
+++ src/components/forms/login.tsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+
+interface formData {
+  email: string;
+  password: string;
+}
+interface Props {
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  submitForm: () => void;
+  inputValue: formData;
+}
+export default function Form({ handleChange, submitForm, inputValue }: Props) {
+  return (
+    <div className="rounded-lg bg-white px-6 py-12 shadow-xl shadow-black/10">
+      <form className="block gap-4">
+        <div>
+          <input
+            type="text"
+            value={inputValue.email}
+            onChange={handleChange}
+            placeholder="Email"
+            name="email"
+            className="my-4 w-80 rounded-sm bg-gray-300 px-2 py-3 outline-gray-400 placeholder:text-slate-600"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            value={inputValue.password}
+            onChange={handleChange}
+            placeholder="Password"
+            name="password"
+            className="my-4 w-80 rounded-sm bg-gray-300 px-2 py-3 outline-gray-400 placeholder:text-slate-600"
+          />
+        </div>
+        <Link to="/admin">
+          <button
+            type="button"
+            onClick={submitForm}
+            className="my-4 w-80 rounded-sm bg-black/90 py-3 font-semibold text-white hover:shadow-lg"
+          >
+            Login
+          </button>
+        </Link>
+      </form>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,31 @@
-import { Content } from "../components/Content";
-
+import { useState } from "react";
+import Form from "../components/forms/login";
 export default function LoginPage() {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+  const submitForm = () => {
+    formData.email != "" &&
+      alert(`Good morning ${formData.email} ${formData.password}`);
+    setFormData({ email: "", password: "" });
+  };
   return (
     <>
-      <Content title="Login" />
-      <p>Karera</p>
+      <div id="exseven">
+        <div className="flex h-screen items-center justify-center bg-gray-100">
+          <Form
+            handleChange={handleChange}
+            submitForm={submitForm}
+            inputValue={formData}
+          />
+        </div>
+      </div>
     </>
   );
 }
